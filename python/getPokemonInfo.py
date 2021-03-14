c6517d0cb86fde10883c72ef454bc9971c8d329b
John Capobianco <ptcapo@gmail.com>
2021-03-14 18:45:14 -0400

    refresh charizard file

--- python/getPokemonInfo.py
+++ python/getPokemonInfo.py
@@ -23,6 +23,7 @@ pokemon_info_template = env.get_template('pokemon_info_template.j2')
 # -------------------------
 pokemon_name = input("Please type in the name of the Pokemon you are trying to catch:")
 url_string =  'https://pokeapi.co/api/v2/pokemon/{0}'.format(pokemon_name)
+
 print (url_string)
 # -------------------------
 # GET Single Pokemon 
@@ -36,3 +37,9 @@ single_pokemon_results_json = single_pokemon_results.json()
 output_from_parsed_pokemon_info_template = pokemon_info_template.render(results=single_pokemon_results_json)
 
 print(output_from_parsed_pokemon_info_template)
+
+# ---------------------------------------
+# Create Files
+# ---------------------------------------
+with open('../output/Text/{0}.csv'.format(pokemon_name), 'w') as fh:
+    fh.write(output_from_parsed_pokemon_info_template)
